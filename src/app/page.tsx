e4a53d3d8d46fd942651786bf6b6ab84127f7111
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-09-05 16:35:39 -0300

    feat(2h02): iniciando use-create-workspace-modal store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 "use client";
+import React from "react";
 import { UserButton } from "@/features/auth/components/user-button";
 
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+
 export default function Номе() {
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = React.useMemo(() => data?.[0]?._id, [data]);
+
+  React.useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+    if (workspaceId) {
+      console.log("Redirect to workspace");
+    } else {
+      console.log("Open creation modal");
+    }
+  }, [isLoading, workspaceId]);
+
   return (
     <div>
       <UserButton />
