242698e5616f4662d7747fcc6f726fb7f7ed3441
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-09-05 05:59:23 -0300

    chore: iniciando convex auth

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      test
+    </main>
+  );
+};
+
+export default TestPage;
