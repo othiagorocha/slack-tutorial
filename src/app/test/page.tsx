d9be6709fbf2ad8d80f68399fb86503047ded60c
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-09-05 08:11:50 -0300

    chore: configurando middleware

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,7 @@
-"use client";
 import React from "react";
 
 const TestPage = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      test
-    </main>
-  );
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24">test</main>;
 };
 
 export default TestPage;
