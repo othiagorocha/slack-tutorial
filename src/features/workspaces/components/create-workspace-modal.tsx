0e105613bd2d594fa38acde8976f4d27c9904438
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-09-06 08:26:26 -0300

    feat: campo de entrada em CreateWorkspaceModal

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -8,13 +8,26 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/use-create-workspace";
 
 export const CreateWorkspaceModal = () => {
   const [open, setOpen] = useCreateWorkspaceModal();
+  const { mutate } = useCreateWorkspace();
 
   const handleClose = () => {
     setOpen(false);
     // TODO: Clear form
+  };
+
+  const handleSubmit = async () => {
+    const data = await mutate(
+      { name: "Workspace 1" },
+      {
+        onError(error) {},
+      }
+    );
   };
 
   return (
@@ -23,6 +36,19 @@ export const CreateWorkspaceModal = () => {
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
+        <form className="space-y-4">
+          <Input
+            value={""}
+            disabled={false}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'."
+          />
+          <div className="flex justify-end">
+            <Button disabled={false}>Create</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
