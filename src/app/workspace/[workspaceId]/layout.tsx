fab1ae0629fe04f6f5116061eeed5112a194b38b
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-09-06 11:42:42 -0300

    feat(3h05): Finalizando componente Toolbar

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,11 +1,18 @@
+"use client";
 import React from "react";
+import { Toolbar } from "./toolbar";
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
 }
 
 const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
-  return <div className="h-full">{children}</div>;
+  return (
+    <div className="h-full">
+      <Toolbar />
+      {children}
+    </div>
+  );
 };
 
 export default WorkspaceIdLayout;
