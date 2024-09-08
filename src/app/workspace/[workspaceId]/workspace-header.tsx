5c9c73ab494ebeb82daaa35cb7e216c5e021a99b
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-09-07 23:41:51 -0300

    feat(4h07): implementando WorkspaceHeader

---
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Doc } from "../../../../convex/_generated/dataModel";
+import { ChevronDown } from "lucide-react";
+
+interface WorkspaceHeaderProps {
+  workspace: Doc<"workspaces">;
+}
+
+export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
+  return (
+    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant={"transparent"}
+            className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+            size={"sm"}
+          >
+            <span className="truncate">{workspace.name}</span>
+            <ChevronDown className="size-4 ml-1 shrink-0" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="bottom" align="start" className="w-64">
+          <DropdownMenuItem className="capitalize">
+            <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2">
+              {workspace.name.charAt(0).toUpperCase()}
+            </div>
+            <div className="flex flex-col items-start">
+              <p className="font-bold">{workspace.name}</p>
+              <p className="text-xs text-muted-foreground">Active workspace</p>
+            </div>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="py-2" onClick={() => {}}>
+            Invite people to {workspace.name}
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="py-2" onClick={() => {}}>
+            Preferences
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,5 +1,38 @@
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { AlertTriangle, Loader } from "lucide-react";
 import React from "react";
+import { WorkspaceHeader } from "./workspace-header";
 
 export const WorkspaceSidebar = () => {
-  return <div>WorkspaceSidebar</div>;
+  const workspaceId = useWorkspaceId();
+
+  // prettier-ignore
+  const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
+  // prettier-ignore
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+
+  if (memberLoading || workspaceLoading) {
+    return (
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-white" />
+      </div>
+    );
+  }
+
+  if (!member || !workspace) {
+    return (
+      <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+        <AlertTriangle className="size-5  text-white" />
+        <p className="text-white text-sm">Workspace not found</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col bg-[#5E2C5F] h-full">
+      <WorkspaceHeader workspace={workspace} />
+    </div>
+  );
 };
