290fa84a12a7fed31bcb643ea5adcbfe7bcbc2a9
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-09-09 09:02:13 -0300

    feat(5h20): implementando componente WorkspaceSection

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,40 +1,48 @@
-import { useCurrentMember } from "@/features/members/api/use-current-member";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { AlertTriangle, Loader, MessageSquareText, SendHorizonal } from "lucide-react";
 import React from "react";
-import { WorkspaceHeader } from "./workspace-header";
+import { AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizonal } from "lucide-react";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { SidebarItem } from "./sidebar-item";
+import { WorkspaceHeader } from "./workspace-header";
+import { WorkspaceSection } from "./workspace-section";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
 
   const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
 
   if (memberLoading || workspaceLoading) {
     return (
-      <div className='flex flex-col bg-[#5E2C5F] h-full items-center justify-center'>
-        <Loader className='size-5 animate-spin text-white' />
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-white" />
       </div>
     );
   }
 
   if (!member || !workspace) {
     return (
-      <div className='flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center'>
-        <AlertTriangle className='size-5  text-white' />
-        <p className='text-white text-sm'>Workspace not found</p>
+      <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+        <AlertTriangle className="size-5  text-white" />
+        <p className="text-white text-sm">Workspace not found</p>
       </div>
     );
   }
 
   return (
-    <div className='flex flex-col bg-[#5E2C5F] h-full'>
+    <div className="flex flex-col bg-[#5E2C5F] h-full">
       <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
-      <div className='flex flex-col px-2 mt-3'>
-        <SidebarItem label='Threads' icon={MessageSquareText} id='threads' />
-        <SidebarItem label='Drafts & Sent' icon={SendHorizonal} id='drafts' />
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
+        <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="drafts" />
+        <WorkspaceSection label="Channels" hint="New channel" onNew={() => {}}>
+          {channels?.map((item) => (
+            <SidebarItem key={item._id} icon={HashIcon} label={item.name} id={item._id} />
+          ))}
+        </WorkspaceSection>
       </div>
     </div>
   );
