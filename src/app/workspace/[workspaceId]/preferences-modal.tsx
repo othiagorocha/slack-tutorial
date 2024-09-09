1173da1454484fcf657fb8064110923e22c3a19d
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-09-09 07:55:45 -0300

    wip editation

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -75,26 +75,26 @@ export const PreferencesModal = ({ open, setOpen, initialValue }: PreferencesMod
     <>
       <ConfirmDialog />
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className='p-0 bg-gray-50 overflow-hidden'>
-          <DialogHeader className='p-4 border-b bg-white'>
+        <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+          <DialogHeader className="p-4 border-b bg-white">
             <DialogTitle>{value}</DialogTitle>
           </DialogHeader>
-          <div className='px-4 pb-4 flex flex-col gap-y-4'>
+          <div className="px-4 pb-4 flex flex-col gap-y-4">
             <Dialog open={editOpen} onOpenChange={setEditOpen}>
               <DialogTrigger asChild>
-                <div className='px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50'>
-                  <div className='flex items-center justify-between'>
-                    <p className='text-sm font-semibold'>Workspace name</p>
-                    <p className='text-sm text-[#1264a3] hover:underline font-semibold'>Edit</p>
+                <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-semibold">Workspace name</p>
+                    <p className="text-sm text-[#1264a3] hover:underline font-semibold">Edit</p>
                   </div>
-                  <p className='text-sm'>{value}</p>
+                  <p className="text-sm">{value}</p>
                 </div>
               </DialogTrigger>
               <DialogContent>
                 <DialogHeader>
                   <DialogTitle>Rename this workspace</DialogTitle>
                 </DialogHeader>
-                <form className='space-y-4' onSubmit={handleEdit}>
+                <form className="space-y-4" onSubmit={handleEdit}>
                   <Input
                     value={value}
                     disabled={isUpdatingWorkspace}
@@ -107,7 +107,7 @@ export const PreferencesModal = ({ open, setOpen, initialValue }: PreferencesMod
                   />
                   <DialogFooter>
                     <DialogClose asChild>
-                      <Button variant='outline' disabled={isUpdatingWorkspace}>
+                      <Button variant="outline" disabled={isUpdatingWorkspace}>
                         Cancel
                       </Button>
                     </DialogClose>
@@ -119,9 +119,10 @@ export const PreferencesModal = ({ open, setOpen, initialValue }: PreferencesMod
             <button
               disabled={isRemovingWorkspace}
               onClick={handleRemove}
-              className='flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer hover:bg-gray-50 text-rose-600'>
-              <TrashIcon className='size-4' />
-              <p className='text-sm font-semibold'>Delete workspace</p>
+              className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer hover:bg-gray-50 text-rose-600"
+            >
+              <TrashIcon className="size-4" />
+              <p className="text-sm font-semibold">Delete workspace</p>
             </button>
           </div>
         </DialogContent>
