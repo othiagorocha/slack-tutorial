ce91494becaf52fdefeb6fad2d53745bffb39455
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-09-16 02:14:22 -0300

    feat(6h45): useEffect em WorkspaceIdPage

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,49 @@
 "use client";
-
+import { useMemo, useEffect } from "react";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
+import { Loader, TriangleAlert } from "lucide-react";
 
 const WorkspaceIdPage = () => {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
+  const [open, setOpen] = useCreateChannelModal();
 
-  return <div>Workspace ID Page</div>;
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
+
+  const channelId = useMemo(() => channels?.[0]?._id, [channels]);
+
+  useEffect(() => {
+    if (workspaceLoading || channelsLoading || !workspace) return;
+    if (channelId) {
+      router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [channelId, workspaceLoading, channelsLoading, workspace, open, setOpen, router, workspaceId]);
+
+  if (workspaceLoading || channelsLoading) {
+    return (
+      <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
+        <Loader className='size-6 animate-spin text-muted-foreground' />
+      </div>
+    );
+  }
+
+  if (!workspace) {
+    return (
+      <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
+        <TriangleAlert className='size-6 text-muted-foreground' />
+        <span className='text-sm text-muted-foreground'>Workspace not found</span>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default WorkspaceIdPage;
