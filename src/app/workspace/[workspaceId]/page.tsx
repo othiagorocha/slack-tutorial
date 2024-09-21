d9ad9eeb5245bbc4f71fc48fa7005c2f0fc8ecc5
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-09-21 09:45:15 -0300

    feat(6h54): implementando verificaçoes em WorkspaceIdPage

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -6,27 +6,42 @@ import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useRouter } from "next/navigation";
 import { Loader, TriangleAlert } from "lucide-react";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
 
 const WorkspaceIdPage = () => {
   const router = useRouter();
   const workspaceId = useWorkspaceId();
   const [open, setOpen] = useCreateChannelModal();
 
+  const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
   const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
 
   const channelId = useMemo(() => channels?.[0]?._id, [channels]);
+  const isAdmin = useMemo(() => member?.role === "admin", [member?.role]);
 
   useEffect(() => {
-    if (workspaceLoading || channelsLoading || !workspace) return;
+    if (workspaceLoading || channelsLoading || memberLoading || !member || !workspace) return;
     if (channelId) {
       router.push(`/workspace/${workspaceId}/channel/${channelId}`);
-    } else if (!open) {
+    } else if (!open && isAdmin) {
       setOpen(true);
     }
-  }, [channelId, workspaceLoading, channelsLoading, workspace, open, setOpen, router, workspaceId]);
+  }, [
+    memberLoading,
+    member,
+    isAdmin,
+    channelId,
+    workspaceLoading,
+    channelsLoading,
+    workspace,
+    open,
+    setOpen,
+    router,
+    workspaceId,
+  ]);
 
-  if (workspaceLoading || channelsLoading) {
+  if (workspaceLoading || channelsLoading || memberLoading) {
     return (
       <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
         <Loader className='size-6 animate-spin text-muted-foreground' />
@@ -34,7 +49,7 @@ const WorkspaceIdPage = () => {
     );
   }
 
-  if (!workspace) {
+  if (!workspace || !member) {
     return (
       <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
         <TriangleAlert className='size-6 text-muted-foreground' />
@@ -43,7 +58,12 @@ const WorkspaceIdPage = () => {
     );
   }
 
-  return null;
+  return (
+    <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
+      <TriangleAlert className='size-6 text-muted-foreground' />
+      <span className='text-sm text-muted-foreground'>No channel found</span>
+    </div>
+  );
 };
 
 export default WorkspaceIdPage;
